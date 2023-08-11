25dc2d4b15a26aa8e5966b3d075ec96bdb91e746
Vinicius <karvvinicius@gmail.com>
2023-08-11 23:20:57 +0100

    add icons navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,8 @@
-import './navbar.css'
+import github from './icons/icons8-github-50.png';
+import email from './icons/icons8-email-50.png';
+import linkedin from './icons/icons8-linkedin-50.png';
+
+import './navbar.css';
 
 function NavBar() {
   return (
@@ -10,8 +14,16 @@ function NavBar() {
                 <li>ANIMATIONS</li>
                 <li>CONTACTO</li>
             </ul>
-            <div>
-
+            <div className="ULNavbar">
+              <a href="https://www.linkedin.com/in/vinicius-carvalho-lucius/">
+                <img src={linkedin} alt="icon linkedin"/>
+              </a>
+              <a href="https://github.com/Vinicuskarv">
+                <img src={github} alt="icon github"/>
+              </a>
+              <a href="https://mail.google.com/mail/u/0/#inbox">
+                <img src={email} alt="icon email"/>
+              </a>
             </div>
         </div>
     </div>
