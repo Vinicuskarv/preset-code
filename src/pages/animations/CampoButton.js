abbbab3029da518db3b6770c75f14a23fab901db
Vinicius <karvvinicius@gmail.com>
2023-08-18 20:29:27 +0100

    add buttons

--- src/pages/animations/CampoButton.js
+++ src/pages/animations/CampoButton.js
@@ -4,20 +4,147 @@ function CampoButton() {
   return (
     <div className='CampoInfoButtons'>
         <div className='container'>
-            <h2>Hover</h2>
-            <div className='d-flex row'>
+          <h2>Buttons</h2>
+          <div className='KitExple'>
+            <h4>Rounded Buttons</h4>
+            <div className='row Rounded'>
             <PreButtons
-                Value= "Nadadem"
-                Descricao="nada tbm"
-                Id = "1"
+                Value= "OUTLINE"
+                Descricao= "OUTLINE"
+                Id = "Rounded1"
             />
             <PreButtons
-                Value= "Nada"
-                Descricao="nada tbm"
-                Id = "2"
+                Value= "NORMAL"
+                Descricao="NORMAL"
+                Id = "Rounded2"
             />
-            </div>
-
+            <PreButtons
+                Value= "HOVER"
+                Descricao="HOVER"
+                Id = "Rounded3"
+            />
+            <PreButtons
+                Value= "FOCUSED"
+                Descricao="FOCUSED"
+                Id = "Rounded4"
+            />
+            <PreButtons
+                Value= "PRESSED"
+                Descricao="PRESSED"
+                Id = "Rounded5"
+            />
+            <PreButtons
+                Value= "DISABLED"
+                Descricao="DISABLED"
+                Id = "Rounded6"
+            />
+          </div>
+          </div>
+          <div className='KitExple'>
+            <h4>Radius Buttons</h4>
+            <div className='row Radius'>
+            <PreButtons
+                Value= "OUTLINE"
+                Descricao="OUTLINE"
+                Id = "Radius1"
+            />
+            <PreButtons
+                Value= "NORMAL"
+                Descricao="NORMAL"
+                Id = "Radius2"
+            />
+            <PreButtons
+                Value= "HOVER"
+                Descricao="HOVER"
+                Id = "Radius3"
+            />
+            <PreButtons
+                Value= "FOCUSED"
+                Descricao="FOCUSED"
+                Id = "Radius4"
+            />
+            <PreButtons
+                Value= "PRESSED"
+                Descricao="PRESSED"
+                Id = "Radius5"
+            />
+            <PreButtons
+                Value= "DISABLED"
+                Descricao="DISABLED"
+                Id = "Radius6"
+            />
+          </div>
+          </div>
+          <div className='KitExple'>
+            <h4>Squared Buttons</h4>
+            <div className='row Squared'>
+            <PreButtons
+                Value= "OUTLINE"
+                Descricao="OUTLINE"
+                Id = "Squared1"
+            />
+            <PreButtons
+                Value= "NORMAL"
+                Descricao="NORMAL"
+                Id = "Squared2"
+            />
+            <PreButtons
+                Value= "HOVER"
+                Descricao="HOVER"
+                Id = "Squared3"
+            />
+            <PreButtons
+                Value= "FOCUSED"
+                Descricao="FOCUSED"
+                Id = "Squared4"
+            />
+            <PreButtons
+                Value= "PRESSED"
+                Descricao="PRESSED"
+                Id = "Squared5"
+            />
+            <PreButtons
+                Value= "DISABLED"
+                Descricao="DISABLED"
+                Id = "Squared6"
+            />
+          </div>
+          </div>
+          <div className='KitExple'>
+            <h4>Squared With Rounded Corners Buttons</h4>
+            <div className='row Corners'>
+            <PreButtons
+                Value= "OUTLINE"
+                Descricao="OUTLINE"
+                Id = "Corners1"
+            />
+            <PreButtons
+                Value= "NORMAL"
+                Descricao="NORMAL"
+                Id = "Corners2"
+            />
+            <PreButtons
+                Value= "HOVER"
+                Descricao="HOVER"
+                Id = "Corners3"
+            />
+            <PreButtons
+                Value= "FOCUSED"
+                Descricao="FOCUSED"
+                Id = "Corners4"
+            />
+            <PreButtons
+                Value= "PRESSED"
+                Descricao="PRESSED"
+                Id = "Corners5"
+            />
+            <PreButtons
+                Value= "DISABLED"
+                Descricao="DISABLED"
+                Id = "Corners6"
+            />
+          </div>
+          </div>
         </div>
     </div>
   );
