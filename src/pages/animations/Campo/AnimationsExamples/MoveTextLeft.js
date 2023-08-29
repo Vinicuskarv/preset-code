5f392af0023d162d17c3b11e49e67d7d68fe29b1
Vinicius <karvvinicius@gmail.com>
2023-08-29 20:19:06 +0100

    add animation text move

---
+++ src/pages/animations/Campo/AnimationsExamples/MoveTextLeft.js
@@ -0,0 +1,11 @@
+import './MoveTextLeft.css';
+
+function MoveTextLeft() {
+    return (
+        <div>
+            <p className="CampoP">Front-end developer.</p>
+        </div>
+    );
+  }
+  
+  export default MoveTextLeft;
--- src/pages/animations/Campo/CampoAnimations.js
+++ src/pages/animations/Campo/CampoAnimations.js
@@ -1,14 +1,54 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { coy } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import './CampoMaster.css';
+import MoveTextLeft from './AnimationsExamples/MoveTextLeft';
+
 
 function CampoAnimations() {
+
+const Animation1 =
+`
+<div>
+    <p class="CampoP">Front-end developer.</p>
+</div>
+<style>
+  .CampoP{
+    color: rgb(0, 0, 0);
+    width: 100%;
+    position: relative;
+    animation: moveTextStart 5s linear infinite;
+  }
+  @keyframes moveTextStart {
+  0% {
+    left: 0;
+  }
+  50%{
+    left: 50px;
+  }
+  100% {
+    left: 0px;
+  }
+}
+</style>
+`;
+
     return (
         <div className='CampoInfoMaster'>
             <div className='container'>
             <h2>Animations</h2>
             </div>
             <div className='KitExple'>
-                <h4>oi</h4>
+                <h4>Modern Typing Effect</h4>
+                <MoveTextLeft/>
             </div>
+            <div className='KitExple'>
+                <div className='CampoCode'>
+                    <SyntaxHighlighter language="html" style={coy}>
+                        {Animation1}
+                    </SyntaxHighlighter>
+                </div> 
+            </div>
+            <br/>
         </div>
     );
   }
